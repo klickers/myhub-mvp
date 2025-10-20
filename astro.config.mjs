016e8356cbe6d9b4d86118e01eee8eeff9d8ea15
Catherine Chen <bluebirid228@gmail.com>
2025-10-20 12:01:09 +0300

    add db

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@astrojs/react"
+import db from "@astrojs/db"
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
-});
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [react(), db()],
+})
