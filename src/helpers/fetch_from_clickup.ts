4da248778123a17dde65f75af1c5746b32d7e779
Catherine Chen <bluebirid228@gmail.com>
2025-10-20 11:18:49 +0300

    add clickup api helper

---
+++ src/helpers/fetch_from_clickup.ts
@@ -0,0 +1,13 @@
+export default async function fetchFromClickUp(url: string) {
+	const options = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization: import.meta.env.CLICKUP_PERSONAL_API_TOKEN,
+		},
+	}
+
+	const res = await fetch("https://api.clickup.com/api/v2" + url, options)
+	const json = await res.json()
+	return json
+}
