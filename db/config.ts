804417105ef5edbd3b562fcc0d2b60dc4786c786
Catherine Chen <bluebirid228@gmail.com>
2025-10-24 16:06:30 +0300

    add weekly task separating

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,21 @@
-import { defineDb } from "astro:db"
+import { column, defineTable, defineDb } from "astro:db"
 
-// https://astro.build/db/config
+const Task = defineTable({
+	columns: {
+		id: column.number({ primaryKey: true }),
+		clickupId: column.text({ unique: true, optional: true }),
+		name: column.text(),
+		status: column.text({ enum: ["unstarted", "", ""] }),
+		startDate: column.date({ optional: true }),
+		dueDate: column.date({ optional: true }),
+		timeEstimate: column.number({ default: 0 }),
+	},
+	//indexes: [{ on: ["clickupId"], unique: true }],
+})
+
+
 export default defineDb({
-	tables: {},
+	tables: {
+		Task,
+	},
 })
