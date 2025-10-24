0339634597a219ad2a8a8ae870d3f1fac82ad95e
Catherine Chen <bluebirid228@gmail.com>
2025-10-24 20:21:22 +0300

    add icon package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from "astro/config"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@astrojs/react"
 import db from "@astrojs/db"
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
-	integrations: [react(), db()],
+	integrations: [react(), db(), icon()],
 })
