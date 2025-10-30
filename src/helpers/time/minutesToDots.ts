c6a9f368f5e90c6a483de62c8f08b2a10866be77
Catherine Chen <bluebirid228@gmail.com>
2025-10-30 23:52:26 +0200

    added method for minutes < 0

--- src/helpers/time/minutesToDots.ts
+++ src/helpers/time/minutesToDots.ts
@@ -1,4 +1,6 @@
 export const minutesToDots = (minutes: number): string => {
+	if (minutes < 0) return "00:00"
+
 	const hours = Math.floor(minutes / 60)
 	const mins = minutes % 60
 	return `${hours < 10 ? "0" + hours : hours}:${String(mins).padStart(
