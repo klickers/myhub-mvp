2511b45f9019be6339454c925a52b9d4b5f9b8fc
Catherine Chen <bluebirid228@gmail.com>
2025-11-13 23:10:00 +0200

    add neon adapter

--- src/helpers/prisma.ts
+++ src/helpers/prisma.ts
@@ -1,8 +1,13 @@
 import { PrismaClient } from "@/generated/prisma/client"
-import { withAccelerate } from "@prisma/extension-accelerate"
 
-const prisma = new PrismaClient({
-	datasourceUrl: import.meta.env.DATABASE_URL,
-}).$extends(withAccelerate())
+// import { withAccelerate } from "@prisma/extension-accelerate"
+// const prisma = new PrismaClient({
+// 	datasourceUrl: import.meta.env.DATABASE_URL,
+// }).$extends(withAccelerate())
+
+import { PrismaNeon } from "@prisma/adapter-neon"
+const connectionString = import.meta.env.DATABASE_URL
+const adapter = new PrismaNeon({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
 export default prisma
