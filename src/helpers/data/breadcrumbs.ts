2e968535b4493a6acb76c1d219cf1e4a333ff454
Catherine Chen <bluebirid228@gmail.com>
2026-04-13 23:17:06 +0800

    add breadcrumbs to each page

---
+++ src/helpers/data/breadcrumbs.ts
@@ -0,0 +1,15 @@
+export const HomeBreadcrumb = {
+		icon: "mingcute:home-4-fill",
+		name: "Home",
+		href: "/",
+	},
+	GuildHallBreadcrumb = {
+		icon: "mingcute:building-4-fill",
+		name: "Guild Hall",
+		href: "/hall",
+	},
+	AlchemyLabBreadcrumb = {
+		icon: "mingcute:flask-3-fill",
+		name: "Alchemy Lab",
+		href: "/lab",
+	}
