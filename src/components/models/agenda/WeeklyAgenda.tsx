560ae1117dd30c6d84584a78547f49662e102d8d
Catherine Chen <bluebirid228@gmail.com>
2026-09-24 23:43:27 +0300

    implement viewing agenda by week

--- src/components/models/agenda/WeeklyAgenda.tsx
+++ src/components/models/agenda/WeeklyAgenda.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useMemo } from "react"
-import { isSameDay, isToday } from "date-fns"
+import { useEffect, useMemo, useState } from "react"
+import { isSameDay, isSameWeek, isToday, addDays } from "date-fns"
+import { Icon } from "@iconify/react"
 import { useAgendaStore } from "@/stores/agenda"
 import { useTagsStore } from "@/stores/tags"
 import { useTasksStore } from "@/stores/tasks"
@@ -9,7 +10,6 @@ import buildTaskTree from "@/helpers/buildTaskTree"
 import type { TaskNode } from "@/types/prisma-custom"
 
 interface Props {
-	date?: Date
 	filter:
 		| { type: "all" }
 		| { type: "tag"; id: number }
@@ -17,7 +17,9 @@ interface Props {
 		| { type: "group"; id: number }
 }
 
-export default function WeeklyAgenda({ date = new Date(), filter }: Props) {
+export default function WeeklyAgenda({ filter }: Props) {
+	const [date, setDate] = useState(new Date())
+
 	const getWeekKey = useAgendaStore((state) => state.getWeekKey)
 	const loadAgendaWeek = useAgendaStore((state) => state.loadAgendaWeek)
 	const agendaByWeek = useAgendaStore((state) => state.agendaByWeek)
@@ -29,8 +31,11 @@ export default function WeeklyAgenda({ date = new Date(), filter }: Props) {
 
 	useEffect(() => {
 		loadAgendaWeek(date)
+	}, [date, loadAgendaWeek])
+
+	useEffect(() => {
 		loadTags()
-	}, [date, loadAgendaWeek, loadTags])
+	}, [loadTags])
 
 	const weekKey = useMemo(() => getWeekKey(date), [getWeekKey, date])
 
@@ -78,63 +83,96 @@ export default function WeeklyAgenda({ date = new Date(), filter }: Props) {
 		})
 	}, [agenda, filter, tags, taskTree])
 
-	return filter.type !== "group" ? (
-		<div className="grid grid-cols-7 mb-6 text-xs">
-			{days.map((day, index) => (
-				<DayAgenda
-					key={index}
-					date={day.date}
-					items={agendaItems.filter((item) =>
-						isSameDay(item.date, day.date),
-					)}
-					dropId={`${filter.type}-${day.date.toISOString()}`}
-					addTaskId={filter.type === "task" ? filter.id : undefined}
-				/>
-			))}
-		</div>
-	) : (
-		<div className="flex flex-col mb-6">
-			<div className="grid grid-cols-8 text-xs">
-				<div></div>
-				{days.map((day, index) => (
-					<div
-						className={
-							"px-1" + (isToday(day.date) ? " bg-yellow-50" : "")
-						}
-						key={index}
-					>
-						<DayAgendaHeader date={day.date} />
-					</div>
-				))}
-			</div>
-			{tags.map((tag) => (
-				<div
-					key={tag.id}
-					className="grid grid-cols-8 text-xs"
+	return (
+		<>
+			{/* Controls */}
+			<div className="flex gap-1 items-center mb-4 justify-end">
+				<button
+					className="px-1 py-1 rounded-lg border border-gray-300 hover:bg-gray-100"
+					onClick={() => setDate(addDays(date, -7))}
 				>
-					<a
-						href={`/tags/${tag.slug}`}
-						className="pr-1 cursor-pointer hover:underline"
-					>
-						{tag.name}
-					</a>
+					<Icon icon="mingcute:left-fill" />
+				</button>
+				<button
+					className={
+						"px-2 py-1 rounded-lg border border-gray-300 hover:bg-gray-100 text-xs uppercase" +
+						(isSameWeek(date, new Date()) ? " bg-gray-100" : "")
+					}
+					onClick={() => setDate(new Date())}
+				>
+					Today
+				</button>
+				<button
+					className="px-1 py-1 rounded-lg border border-gray-300 hover:bg-gray-100"
+					onClick={() => setDate(addDays(date, 7))}
+				>
+					<Icon icon="mingcute:right-fill" />
+				</button>
+			</div>
+
+			{filter.type !== "group" ? (
+				<div className="grid grid-cols-7 mb-6 text-xs">
 					{days.map((day, index) => (
 						<DayAgenda
 							key={index}
 							date={day.date}
-							dropId={`tag-${tag.id}-${day.date.toISOString()}`}
-							showHeader={false}
-							items={agendaItems.filter(
-								(item) =>
-									isSameDay(item.date, day.date) &&
-									item.task?.tags.some(
-										(taskTag) => taskTag.tagId === tag.id,
-									),
+							items={agendaItems.filter((item) =>
+								isSameDay(item.date, day.date),
 							)}
+							dropId={`${filter.type}-${day.date.toISOString()}`}
+							addTaskId={
+								filter.type === "task" ? filter.id : undefined
+							}
 						/>
 					))}
 				</div>
-			))}
-		</div>
+			) : (
+				<div className="flex flex-col mb-6">
+					<div className="grid grid-cols-8 text-xs">
+						<div></div>
+						{days.map((day, index) => (
+							<div
+								className={
+									"px-1" +
+									(isToday(day.date) ? " bg-yellow-50" : "")
+								}
+								key={index}
+							>
+								<DayAgendaHeader date={day.date} />
+							</div>
+						))}
+					</div>
+					{tags.map((tag) => (
+						<div
+							key={tag.id}
+							className="grid grid-cols-8 text-xs"
+						>
+							<a
+								href={`/tags/${tag.slug}`}
+								className="pr-1 cursor-pointer hover:underline"
+							>
+								{tag.name}
+							</a>
+							{days.map((day, index) => (
+								<DayAgenda
+									key={index}
+									date={day.date}
+									dropId={`tag-${tag.id}-${day.date.toISOString()}`}
+									showHeader={false}
+									items={agendaItems.filter(
+										(item) =>
+											isSameDay(item.date, day.date) &&
+											item.task?.tags.some(
+												(taskTag) =>
+													taskTag.tagId === tag.id,
+											),
+									)}
+								/>
+							))}
+						</div>
+					))}
+				</div>
+			)}
+		</>
 	)
 }
